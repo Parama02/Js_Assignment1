7cdbe079a6685c229db6055c22b8037f8bb15349
Parama02 <99189639+Parama02@users.noreply.github.com>
2022-03-11 20:49:04 +0530

    js file

---
+++ script.js
@@ -0,0 +1,39 @@
+
+//Variables 
+
+var StringName="Assignment1"
+var IntNumber=10
+var floatNumber=100.50
+var BooleanValue=true
+
+//console.log to get the output
+
+console.log(StringName)
+console.log(IntNumber)
+console.log(floatNumber)
+console.log(BooleanValue)
+
+// VAR
+
+var pi=3.14
+var radius=2
+var Area= pi*(radius*radius)
+console.log(Area)
+
+var radius=5
+console.log(Area)
+
+// LET
+
+var mulTwo=2
+let circumference= mulTwo*pi*radius
+console.log(circumference)
+
+// CONST
+
+const RadiusOfCircle=radius
+
+console.log(RadiusOfCircle)
+
+
+
