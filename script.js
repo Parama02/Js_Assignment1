6870d2bfdb55be8b06e3da50b71283a2fd04eba6
Parama02 <99189639+Parama02@users.noreply.github.com>
2022-03-12 18:55:06 +0530

    edit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-//Variables 
+//Variables Declaration
 
 var StrName="Assignment1"
 var IntNum=10
-var floatNum=100.50
+var floatNum=100.55
 var BooleanVal=true
 
 //console.log to get the output
@@ -13,7 +13,7 @@ console.log(IntNum)
 console.log(floatNum)
 console.log(BooleanVal)
 
-// VAR
+// using   VAR  to find area of the circle
 
 var pi=3.14
 var r=2
@@ -21,15 +21,16 @@ var Area= pi*(r*r)
 console.log(Area)
 
 var r=5
+var Area= pi*(r*r)
 console.log(Area)
 
-// LET
+// using   LET  to find circumference of the circle
 
 var mulTwo=2
 let circumference= mulTwo*pi*r
 console.log(circumference)
 
-// CONST
+// using  CONST  to find radius 
 
 const RadiusOfCircle=r
 
